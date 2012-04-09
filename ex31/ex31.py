035ed0bf52a8f467c805ef556bcee2af3e17e72b
Chris Higgins <chris@chigs.me>
2012-04-08 21:54:04 -0500

    Fixed indentation

--- ex31/ex31.py
+++ ex31/ex31.py
@@ -10,11 +10,11 @@ if door == "1":
   bear = raw_input("> ")
 
   if bear == "1":
-	print "The bear eats your face off. Good job!"
+    print "The bear eats your face off. Good job!"
   elif bear == "2":
-	print "The bear eats your legs off. Good job!"
+    print "The bear eats your legs off. Good job!"
   else:
-	print "Well, doing %s is probably better. Bear runs away." % bear
+    print "Well, doing %s is probably better. Bear runs away." % bear
 
 elif door == "2":
   print "You stare into the endless abyss at Cthulhu's retina."
@@ -25,9 +25,9 @@ elif door == "2":
   insanity = raw_input("> ")
 
   if insanity == "1" or insanity == "2":
-	print "Your body survives powered by a mind of jello. Good job!"
+    print "Your body survives powered by a mind of jello. Good job!"
   else:
-	print "The insanity rots your eyes into a pool of muck. Good job!"
-	
+    print "The insanity rots your eyes into a pool of muck. Good job!"
+    
 else:
   print "You stumble around and fall on a knife and die. Good job!"
